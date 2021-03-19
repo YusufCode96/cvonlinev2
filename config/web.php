5750f822a9e35e18b0c185c90380a33a8b53ccd6
Yusuf Putra Anugrah <yusufpa97@gmail.com>
2021-03-20 00:50:34 +0700

    perbaikan eror compres file

--- config/web.php
+++ config/web.php
@@ -17,49 +17,49 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
-        'assetsAutoCompress' => [
-            'class'   => '\skeeks\yii2\assetsAuto\AssetsAutoCompressComponent',
-            'enabled' => false,
+        // 'assetsAutoCompress' => [
+        //     'class'   => '\skeeks\yii2\assetsAuto\AssetsAutoCompressComponent',
+        //     'enabled' => false,
 
-            'readFileTimeout' => 3,           //Time in seconds for reading each asset file
+        //     'readFileTimeout' => 3,           //Time in seconds for reading each asset file
 
-            'jsCompress'                => true,        //Enable minification js in html code
-            'jsCompressFlaggedComments' => true,        //Cut comments during processing js
+        //     'jsCompress'                => true,        //Enable minification js in html code
+        //     'jsCompressFlaggedComments' => true,        //Cut comments during processing js
 
-            'cssCompress' => true,        //Enable minification css in html code
+        //     'cssCompress' => true,        //Enable minification css in html code
             
-            'cssFileCompile'        => true,        //Turning association css files
-            'cssFileCompileByGroups' => true,       //Enables the compilation of files in groups rather than in a single file. Works only when the $cssFileCompile option is enabled
-            'cssFileRemouteCompile' => false,       //Trying to get css files to which the specified path as the remote file, skchat him to her.
-            'cssFileCompress'       => true,        //Enable compression and processing before being stored in the css file
-            'cssFileBottom'         => false,       //Moving down the page css files
-            'cssFileBottomLoadOnJs' => false,       //Transfer css file down the page and uploading them using js
+        //     'cssFileCompile'        => true,        //Turning association css files
+        //     'cssFileCompileByGroups' => true,       //Enables the compilation of files in groups rather than in a single file. Works only when the $cssFileCompile option is enabled
+        //     'cssFileRemouteCompile' => false,       //Trying to get css files to which the specified path as the remote file, skchat him to her.
+        //     'cssFileCompress'       => true,        //Enable compression and processing before being stored in the css file
+        //     'cssFileBottom'         => false,       //Moving down the page css files
+        //     'cssFileBottomLoadOnJs' => false,       //Transfer css file down the page and uploading them using js
 
-            'jsFileCompile'                 => true,        //Turning association js files
-            'jsFileCompileByGroups'         => true,        //Enables the compilation of files in groups rather than in a single file. Works only when the $jsFileCompile option is enabled
-            'jsFileRemouteCompile'          => false,       //Trying to get a js files to which the specified path as the remote file, skchat him to her.
-            'jsFileCompress'                => true,        //Enable compression and processing js before saving a file
-            'jsFileCompressFlaggedComments' => true,        //Cut comments during processing js
+        //     'jsFileCompile'                 => true,        //Turning association js files
+        //     'jsFileCompileByGroups'         => true,        //Enables the compilation of files in groups rather than in a single file. Works only when the $jsFileCompile option is enabled
+        //     'jsFileRemouteCompile'          => false,       //Trying to get a js files to which the specified path as the remote file, skchat him to her.
+        //     'jsFileCompress'                => true,        //Enable compression and processing js before saving a file
+        //     'jsFileCompressFlaggedComments' => true,        //Cut comments during processing js
 
-            'noIncludeJsFilesOnPjax' => true,        //Do not connect the js files when all pjax requests when all pjax requests when enabled jsFileCompile
-            'noIncludeCssFilesOnPjax' => true,        //Do not connect the css files when all pjax requests when all pjax requests when enabled cssFileCompile
+        //     'noIncludeJsFilesOnPjax' => true,        //Do not connect the js files when all pjax requests when all pjax requests when enabled jsFileCompile
+        //     'noIncludeCssFilesOnPjax' => true,        //Do not connect the css files when all pjax requests when all pjax requests when enabled cssFileCompile
 
-            'htmlFormatter' => [
-                //Enable compression html
-                'class'         => 'skeeks\yii2\assetsAuto\formatters\html\TylerHtmlCompressor',
-                'extra'         => false,       //use more compact algorithm
-                'noComments'    => true,        //cut all the html comments
-                'maxNumberRows' => 50000,       //The maximum number of rows that the formatter runs on
+        //     'htmlFormatter' => [
+        //         //Enable compression html
+        //         'class'         => 'skeeks\yii2\assetsAuto\formatters\html\TylerHtmlCompressor',
+        //         'extra'         => false,       //use more compact algorithm
+        //         'noComments'    => true,        //cut all the html comments
+        //         'maxNumberRows' => 50000,       //The maximum number of rows that the formatter runs on
 
-                //or
+        //         //or
 
-                //'class' => 'skeeks\yii2\assetsAuto\formatters\html\MrclayHtmlCompressor',
+        //         //'class' => 'skeeks\yii2\assetsAuto\formatters\html\MrclayHtmlCompressor',
 
-                //or any other your handler implements skeeks\yii2\assetsAuto\IFormatter interface
+        //         //or any other your handler implements skeeks\yii2\assetsAuto\IFormatter interface
 
-                //or false
-            ],
-        ],
+        //         //or false
+        //     ],
+        // ],
         'assetManager' => [
     'bundles' => [
         // you can override AssetBundle configs here       
