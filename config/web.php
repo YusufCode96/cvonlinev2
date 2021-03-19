a7df488b3d176d8dd3188bbadc6117586c9ab2ac
Yusuf Putra Anugrah <yusufpa97@gmail.com>
2021-03-20 00:51:47 +0700

    perbaikan eror compres file

--- config/web.php
+++ config/web.php
@@ -11,7 +11,7 @@ $config = [
         ],
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log','assetsAutoCompress'],
+    'bootstrap' => ['log'],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
